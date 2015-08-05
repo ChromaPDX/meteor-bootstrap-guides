99da5a79a9c2a6a3d9e9d28e08cb522213f9cd53
structuresound <leif@structuresound.com>
2015-08-04 17:25:15 -0700

    packagejs

--- package.js
+++ package.js
@@ -1,35 +1,26 @@
 Package.describe({
   name: 'chroma:bootstrap-guides',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'press ctl+g to reveal bootstap vertical column guides',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/ChromaPDX/meteor-bootstrap-guides',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
+  debugOnly: true
 });
 
 Package.on_use(function(api) {
-  // production only
-  if (process.env.IS_PRODUCTION) {
-  }
-  // dev only
-  if (process.env.IS_DEVELOPMENT) {
-    api.versionsFrom('1.1.0.2');
-    api.use([
-      "jquery",
-      "coffeescript",
-      "templating",
-      "mquandalle:jade@0.4.3"
+  api.versionsFrom('1.1.0.2');
+  api.use([
+    "jquery",
+    "coffeescript",
+    "templating",
+    "mquandalle:jade@0.4.3"
   ], "client");
 
-    api.addFiles([
-      'bootstrap-guides.jade',
-      'bootstrap-guides.css',
-      'bootstrap-guides.coffee'
-    ], 'client');
-  }
+  api.addFiles([
+    'bootstrap-guides.jade',
+    'bootstrap-guides.css',
+    'bootstrap-guides.coffee'
+  ], "client");
 });
 
 Package.onTest(function(api) {
